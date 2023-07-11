1bfcb1adb98456ad9abcd736ecb476b7a86e2719
Andreas Svensson <andreas.svensson@iths.se>
2023-07-11 13:08:07 +0200

    created combination function to calculate settings combinations

--- data_augmentation/augment.py
+++ data_augmentation/augment.py
@@ -1,12 +1,31 @@
-import sys
 from config import * # get settings from config file
+
+import itertools
+import math
+import random
+import sys
+
+settings = [setting_one, setting_two] # store all config settings in one list
 
 # TODO: implement augmentation functions
 
-# TODO: implement combinatorics function to go over all combinations of augmentations or a random selection
+def create_combinations(combination_limit, settings):
+    possible_combinations = math.prod([len(setting) for setting in settings]) # unique combinations possible
+
+    if combination_limit: # if a limit was set in config file
+        if possible_combinations > combination_limit: # if all combinations exceeds that limit:
+            # create a random subset of unique combinations to use
+            combinations = random.sample(list(itertools.product(*settings)), combination_limit)
+            return combinations
+        
+    # limit does not exist or is not exceeded -> use all possible combinations
+    combinations = list(itertools.product(setting_one, setting_two))
+    return combinations
+
 
 # TODO: calculate amount of files created based on amount of settings combinations used
-files_created = 101 # placeholder value larger than default setting for now
+files_created = len(setting_one) * len(setting_two) # amount of files that will be created if ALL combinations are used
+
 
 def exceeds_warning_limit(files_created, warning_limit):
     if warning_limit: # if a limit was set in config file
@@ -18,5 +37,9 @@ def exceeds_warning_limit(files_created, warning_limit):
                 print("Script stopped!")
                 sys.exit()
 
+
 if __name__ == "__main__":
     exceeds_warning_limit(files_created, warning_limit)
+
+    combinations = create_combinations(combination_limit, settings)
+    [print(combination) for combination in combinations]
